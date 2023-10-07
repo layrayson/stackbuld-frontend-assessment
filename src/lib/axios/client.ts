2d70da66dbc284f46e1b27ef2ac9bb5818151dd8
Abdulazeez Olalere <olalereazeez11@gmail.com>
2023-10-07 08:28:53 +0100

    FEAT:create axios client

---
+++ src/lib/axios/client.ts
@@ -0,0 +1,32 @@
+import axios, {
+  AxiosInstance,
+  AxiosResponse,
+  AxiosError,
+  InternalAxiosRequestConfig,
+  AxiosRequestConfig,
+} from "axios";
+
+interface XAxiosInstance extends AxiosInstance {
+  request<T = any, R = AxiosResponse<T>>(
+    config: AxiosRequestConfig
+  ): Promise<R>;
+}
+const axiosClient: AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_APP_BASE_URL,
+  timeout: 5000,
+  //   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosClient.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response.data;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosClient;
